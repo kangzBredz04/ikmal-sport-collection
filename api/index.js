379dcc5c1b8cbb2aed1307fa52a79682244964e1
wahyu <wabredz1234@gmail.com>
2024-06-29 13:00:32 +0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://sahaba-fashion.vercel.app"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+const router = express.Router();
+app.use("/api", router);
+
+router.use("/hello", async (_req, res) => {
+  res.status(200).json({
+    message: "Selamat Datang Di Website Sport Collection",
+  });
+});
+
+app.listen(process.env.API_PORT, () =>
+  console.log("Server berhasil dijalankan.")
+);
