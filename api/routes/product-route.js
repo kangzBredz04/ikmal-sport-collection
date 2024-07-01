4bceba0fe9f0e7164bfd8622b4e52d08c7387fd4
wahyu <wabredz1234@gmail.com>
2024-07-01 22:10:13 +0700

    commit

--- api/routes/product-route.js
+++ api/routes/product-route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllProduct } from "../controller/product-controller";
+import { getAllProduct } from "../controller/product-controller.js";
 
 const router = express.Router();
 
