aa1804dddec771e512b006a9d0974b349e609c51
wahyu <wabredz1234@gmail.com>
2024-07-02 23:59:54 +0700

    commti

--- api/controller/product-controller.js
+++ api/controller/product-controller.js
@@ -2,7 +2,7 @@ import { pool } from "../config/db.js";
 
 export const getAllProduct = async (_req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM products");
+    const result = await pool.query("SELECT * FROM product");
     res.json(result.rows);
   } catch (error) {
     res.status(500).json({ msg: error.message });
