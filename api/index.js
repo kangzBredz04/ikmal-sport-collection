14a3b63a75398f31ed04f91652572386ddb06e65
wahyu <wabredz1234@gmail.com>
2024-07-03 21:56:10 +0700

    commit cors

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import "dotenv/config";
-// import cors from "cors";
+import cors from "cors";
 
 import ProductRoute from "./routes/product-route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const router = express.Router();
 app.use("/api", router);
